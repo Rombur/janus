9ae88ae42719790e9440c5f97066c0b7b84ca250
Bruno Turcksin <bruno@Acheron.(none)>
2012-02-23 23:02:52 -0600

    Initial commit.

---
+++ janus.cc
@@ -0,0 +1,15 @@
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc,char** argv)
+{
+  assert(argc==4);
+
+  string output_filename(argv[3]);
+
+  return 0;
+}
